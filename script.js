b5df935b6873ad6556d7610a61a7e5da667e13cb
Anmol <anmol1314@hotmail.com>
2025-07-30 03:39:16 -0700

    added auto clearing when total is last operated number, need to add regular clear button

--- script.js
+++ script.js
@@ -44,25 +44,20 @@ let total = 0;
 
 let operator = "unupd";
 
-// total = operate(a,b, " ");
+
 
 const content = document.querySelector("#content");
 
-// let digit = 9;
-// const row = document.createElement("div");
+
 const display = document.createElement("div");
 display.id = "display";
 display.value = "";
-// display.textContent="123"; //instead maybe input box
-// const nums = document.createElement("div");
-// nums.textContent = "123";
-// nums.id = "nums";
-// display.appendChild(nums);
 content.appendChild(display);
 const btns = document.createElement("div");
 
 //make display functional, and capable storing a value to variable
 //make operators function on display
+//add a clear button
 for(i = 9; i >= 0; i--) {
     // if(i == 9 | i == 6 | i == 3 | i == 0) {
         
@@ -72,6 +67,13 @@ for(i = 9; i >= 0; i--) {
     digit.value = i;
     digit.textContent = i;
     digit.addEventListener("click", function (e) {
+        if(operator == "=") {
+            //reset display
+            document.getElementById("display").value = "";
+            document.getElementById("display").textContent = document.getElementById("display").value;
+            a = undefined;
+            operator = "unupd";
+        }
         //if display contains total do not append to total;
         let old = document.getElementById("display").value;
         // console.log(old);
@@ -85,46 +87,119 @@ for(i = 9; i >= 0; i--) {
 const plus = document.createElement("button");
 plus.textContent = "+";
 plus.addEventListener("click", ()=> {
-    a = document.getElementById("display").value;
-    operator = "+";
-    document.getElementById("display").value = "";
-    document.getElementById("display").textContent = document.getElementById("display").value;
+    if(["+", "-", "x", "/"].includes(operator)) {
+        //do equals functionality
+        b = document.getElementById("display").value; //value of display after operator press
+        // operator = "/";
+        console.log(a + " " + operator + " " + b + "=");
+        total = operate(a,b, operator); //make sure operator changes value with above button presses
+        //display value
+        a = total;
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = a;
+    } else {
+        // if(a == undefined) {
+            
+        // }
+        a = document.getElementById("display").value;
+        operator = "+";
+        //empty display
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = document.getElementById("display").value;
+    }
+    
+    //fill b if pressed again
 })
 const minus = document.createElement("button");
 minus.textContent = "-";
 minus.addEventListener("click", ()=> {
-    a = document.getElementById("display").value;
-    operator = "-";
-    document.getElementById("display").value = "";
-    document.getElementById("display").textContent = document.getElementById("display").value;
-    // console.log(a);
+    if(["+", "-", "x", "/"].includes(operator)) {
+        //do equals functionality
+        b = document.getElementById("display").value; //value of display after operator press
+        // operator = "/";
+        console.log(a + " " + operator + " " + b + "=");
+        total = operate(a,b, operator); //make sure operator changes value with above button presses
+        //display value
+        a = total;
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = a;
+    } else {
+        // if(a == undefined) {
+            
+        // }
+        a = document.getElementById("display").value;
+        operator = "-";
+        //empty display
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = document.getElementById("display").value;
+    }
 })
 const mult = document.createElement("button");
 mult.textContent = "x";
 mult.addEventListener("click", ()=> {
-    a = document.getElementById("display").value;
-    operator = "x";
-    document.getElementById("display").value = "";
-    document.getElementById("display").textContent = document.getElementById("display").value;
+    if(["+", "-", "x", "/"].includes(operator)) {
+        //do equals functionality
+        b = document.getElementById("display").value; //value of display after operator press
+        // operator = "/";
+        console.log(a + " " + operator + " " + b + "=");
+        total = operate(a,b, operator); //make sure operator changes value with above button presses
+        //display value
+        a = total;
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = a;
+    } else {
+        // if(a == undefined) {
+            
+        // }
+        a = document.getElementById("display").value;
+        operator = "x";
+        //empty display
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = document.getElementById("display").value;
+    }
 })
 const div = document.createElement("button");
 div.textContent = "/";
 div.addEventListener("click", ()=> {
-    a = document.getElementById("display").value;
-    operator = "/"; // this should also do equals functionality;
-    document.getElementById("display").value = "";
-    document.getElementById("display").textContent = document.getElementById("display").value;
+    if(["+", "-", "x", "/"].includes(operator)) {
+        //do equals functionality
+        b = document.getElementById("display").value; //value of display after operator press
+        // operator = "/";
+        console.log(a + " " + operator + " " + b + "=");
+        total = operate(a,b, operator); //make sure operator changes value with above button presses
+        //display value
+        a = total;
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = a;
+    } else {
+        // if(a == undefined) {
+            
+        // }
+        a = document.getElementById("display").value;
+        operator = "/";
+        //empty display
+        document.getElementById("display").value = "";
+        document.getElementById("display").textContent = document.getElementById("display").value;
+    }
 })
 
 const equal = document.createElement("button");
 equal.textContent = "=";
 equal.addEventListener("click", ()=> {
-    b = document.getElementById("display").value; //value of display after operator press
-    // operator = "/";
-    console.log(a + " " + operator + " " + b + "=");
-    total = operate(a,b, operator); //make sure operator changes value with above button presses
+    if(operator == "=") {
+        total = document.getElementById("display").value;
+    } else {
+        b = document.getElementById("display").value; //value of display after operator press
+        // operator = "/";
+        console.log(a + " " + operator + " " + b + "=");
+        total = operate(a,b, operator);
+    }
+    operator = "=";
     document.getElementById("display").value = total;
-    document.getElementById("display").textContent = document.getElementById("display").value;
+    document.getElementById("display").textContent = total;
+    // a = total; //reset values after pressing, add functionality where pressing an operator after equals will keep the total as a;
+    b = undefined;
+    // a = total;
     // a = total; //display total;
     console.log(total);
 })
